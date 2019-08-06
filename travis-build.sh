1a250685ff762e06e191977113adb565e6e9cac2
Graeme Rocher <graeme.rocher@gmail.com>
2019-08-06 16:35:59 +0200

    Release 1.2.0

--- travis-build.sh
+++ travis-build.sh
@@ -48,10 +48,7 @@ if [[ $EXIT_STATUS -eq 0 ]]; then
       echo "Publishing archives"
       ./gradlew --stop
       if [[ -n $TRAVIS_TAG ]]; then
-          ./gradlew bintrayUpload --no-daemon --stacktrace || EXIT_STATUS=$?
-          if [[ $EXIT_STATUS -eq 0 ]]; then
-            ./gradlew synchronizeWithMavenCentral --no-daemon
-          fi
+          ./gradlew bintrayUpload --no-daemon --stacktrace || EXIT_STATUS=$?          
       else
           ./gradlew publish --no-daemon --stacktrace || EXIT_STATUS=$?
       fi
@@ -100,7 +97,11 @@ if [[ $EXIT_STATUS -eq 0 ]]; then
         rm -rf gh-pages
       fi
  
-
+      if [[ -n $TRAVIS_TAG ]]; then
+          if [[ $EXIT_STATUS -eq 0 ]]; then
+            ./gradlew synchronizeWithMavenCentral --no-daemon
+          fi
+      fi
     fi
 fi
 
