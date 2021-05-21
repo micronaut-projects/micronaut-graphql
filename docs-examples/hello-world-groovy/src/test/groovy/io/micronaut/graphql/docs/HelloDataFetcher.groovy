e6f457d1c817ddaf26c70116c279c779a46f62ec
Iván López <lopezi@objectcomputing.com>
2021-05-21 12:44:50 +0200

    Extract hello-world examples to docs-examples used in documentation (#191)

---
+++ docs-examples/hello-world-groovy/src/test/groovy/io/micronaut/graphql/docs/HelloDataFetcher.groovy
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2017-2020 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.micronaut.graphql.docs
+
+import graphql.schema.DataFetcher
+import graphql.schema.DataFetchingEnvironment
+import groovy.transform.CompileStatic
+
+import javax.inject.Singleton
+
+/**
+ * @author Marcel Overdijk
+ */
+@Singleton
+@CompileStatic
+class HelloDataFetcher implements DataFetcher<String> {
+
+    @Override
+    String get(DataFetchingEnvironment env) {
+        String name = env.getArgument("name")
+        name = name?.trim() ?: "World"
+        return "Hello ${name}!"
+    }
+}
--- examples/hello-world-groovy/README.md
+++
@@ -1,9 +0,0 @@
-# Micronaut GraphQL Hello World Groovy example
-
-Start the application:
-
-    ./gradlew clean :graphql-example-hello-world-groovy:run
-
-Run GraphQL query:
-
-    curl 'http://localhost:8080/graphql' -H 'content-type: application/json' --data-binary '{"query":"{\n  hello(name: \"Simba\")\n}\n"}'
