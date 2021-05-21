cee4d988cc819fe05a57de8f44d37dab51dde68b
Iván López <lopezi@objectcomputing.com>
2021-05-21 11:37:08 +0200

    Extract hello-world examples to docs-examples used in documentation

---
+++ docs-examples/hello-world-kotlin/src/test/kotlin/io/micronaut/graphql/docs/GraphQLFactory.kt
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2017-2020 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.micronaut.graphql.docs
+
+// tag::imports[]
+import graphql.GraphQL
+import graphql.schema.idl.RuntimeWiring
+import graphql.schema.idl.SchemaGenerator
+import graphql.schema.idl.SchemaParser
+import graphql.schema.idl.TypeDefinitionRegistry
+import io.micronaut.context.annotation.Bean
+import io.micronaut.context.annotation.Factory
+import io.micronaut.core.io.ResourceResolver
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import javax.inject.Singleton
+// end::imports[]
+
+/**
+ * @author Marcel Overdijk
+ */
+@SuppressWarnings("Duplicates")
+// tag::clazz[]
+@Factory // <1>
+class GraphQLFactory {
+
+    @Bean
+    @Singleton
+    fun graphQL(resourceResolver: ResourceResolver, helloDataFetcher: HelloDataFetcher): GraphQL { // <2>
+
+        val schemaParser = SchemaParser()
+        val schemaGenerator = SchemaGenerator()
+
+        // Parse the schema.
+        val typeRegistry = TypeDefinitionRegistry();
+        typeRegistry.merge(schemaParser.parse(BufferedReader(InputStreamReader(
+                resourceResolver.getResourceAsStream("classpath:schema.graphqls").get()))))
+
+        // Create the runtime wiring.
+        val runtimeWiring = RuntimeWiring.newRuntimeWiring()
+                .type("Query") { typeWiring -> typeWiring
+                        .dataFetcher("hello", helloDataFetcher) }
+                .build()
+
+        // Create the executable schema.
+        val graphQLSchema = schemaGenerator.makeExecutableSchema(typeRegistry, runtimeWiring)
+
+        // Return the GraphQL bean.
+        return GraphQL.newGraphQL(graphQLSchema).build()
+    }
+}
+// end::clazz[]
