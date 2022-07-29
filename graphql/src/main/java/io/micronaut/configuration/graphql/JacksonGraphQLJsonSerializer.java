1da6061059e216cbe1595ad2d808197894c16aeb
Dean Wette <wetted@objectcomputing.com>
2022-07-29 13:40:28 -0500

    Remove deprecated code and prepare for Micronaut 4

--- graphql/src/main/java/io/micronaut/configuration/graphql/JacksonGraphQLJsonSerializer.java
+++ graphql/src/main/java/io/micronaut/configuration/graphql/JacksonGraphQLJsonSerializer.java
@@ -15,9 +15,7 @@
  */
 package io.micronaut.configuration.graphql;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import io.micronaut.core.type.Argument;
-import io.micronaut.jackson.databind.JacksonDatabindMapper;
 import io.micronaut.json.JsonMapper;
 import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
@@ -35,17 +33,6 @@ import java.nio.charset.StandardCharsets;
 public class JacksonGraphQLJsonSerializer implements GraphQLJsonSerializer {
 
     private final JsonMapper jsonMapper;
-
-    /**
-     * Default constructor.
-     *
-     * @param objectMapper the {@link ObjectMapper} instance
-     * @deprecated Use {@link #JacksonGraphQLJsonSerializer(JsonMapper)} instead
-     */
-    @Deprecated
-    public JacksonGraphQLJsonSerializer(ObjectMapper objectMapper) {
-        this(new JacksonDatabindMapper(objectMapper));
-    }
 
     /**
      * Default constructor.
