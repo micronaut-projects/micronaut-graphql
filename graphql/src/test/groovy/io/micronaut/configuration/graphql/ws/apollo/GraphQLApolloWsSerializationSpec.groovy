6a7fded57e435216152fbd065c3bc5d81a352b6d
Guillermo Calvo <guillermo@guillermo.dev>
2023-08-07 11:41:11 +0200

    Add more serialization tests

--- graphql/src/test/groovy/io/micronaut/configuration/graphql/ws/apollo/GraphQLApolloWsSerializationSpec.groovy
+++ graphql/src/test/groovy/io/micronaut/configuration/graphql/ws/apollo/GraphQLApolloWsSerializationSpec.groovy
@@ -4,11 +4,22 @@ import io.micronaut.configuration.graphql.GraphQLRequestBody
 import io.micronaut.configuration.graphql.GraphQLResponseBody
 import io.micronaut.jackson.databind.JacksonDatabindMapper
 import io.micronaut.json.JsonMapper
+import io.micronaut.test.extensions.spock.annotation.MicronautTest
+import jakarta.inject.Inject
+import spock.lang.Shared
 import spock.lang.Specification
 
+@MicronautTest(startApplication = false)
 class GraphQLApolloWsSerializationSpec extends Specification {
 
-    void "test request serialization using jackson object mapper"() {
+    @Shared
+    @Inject
+    JsonMapper micronautMapper
+
+    @Shared
+    JsonMapper jacksonMapper = new JacksonDatabindMapper();
+
+    void "test request serialization using #mapperType object mapper"(String mapperType) {
         given:
         GraphQLApolloWsRequest request = new GraphQLApolloWsRequest()
         request.setType(GraphQLApolloWsRequest.ClientType.GQL_CONNECTION_INIT.getType())
@@ -20,19 +31,22 @@ class GraphQLApolloWsSerializationSpec extends Specification {
         request.setPayload(body)
 
         when:
-        JsonMapper jsonMapper = new JacksonDatabindMapper();
         String serializedRequest = jsonMapper.writeValueAsString(request)
 
         then:
         serializedRequest == "{\"type\":\"connection_init\",\"id\":\"test-id\",\"payload\":{\"query\":\"test-query\",\"operationName\":\"test-operation\",\"variables\":{\"test-key\":\"test-value\"}}}"
+
+        where:
+        mapperType  | jsonMapper
+        'jackson'   | jacksonMapper
+        'micronaut' | micronautMapper
     }
 
-    void "test request deserialization using jackson object mapper"() {
+    void "test request deserialization using #mapperType object mapper"(String mapperType) {
         given:
         String request = "{\"type\":\"connection_init\",\"id\":\"test-id\",\"payload\":{\"query\":\"test-query\",\"operationName\":\"test-operation\",\"variables\":{\"test-key\":\"test-value\"}}}"
 
         when:
-        JsonMapper jsonMapper = new JacksonDatabindMapper();
         GraphQLApolloWsRequest deserializedRequest = jsonMapper.readValue(request, GraphQLApolloWsRequest)
 
         then:
@@ -42,27 +56,35 @@ class GraphQLApolloWsSerializationSpec extends Specification {
         deserializedRequest.getPayload().getOperationName() == "test-operation"
         deserializedRequest.getPayload().getVariables().size() == 1
         deserializedRequest.getPayload().getVariables().get("test-key") == "test-value"
+
+        where:
+        mapperType  | jsonMapper
+        'jackson'   | jacksonMapper
+        'micronaut' | micronautMapper
     }
 
-    void "test response serialization using jackson object mapper"() {
+    void "test response serialization using #mapperType object mapper"(String mapperType) {
         given:
         GraphQLResponseBody body = new GraphQLResponseBody(Map.of("test-key", "test-value"))
         GraphQLApolloWsResponse response = new GraphQLApolloWsResponse(GraphQLApolloWsResponse.ServerType.GQL_CONNECTION_ACK, "test-id", body)
 
         when:
-        JsonMapper jsonMapper = new JacksonDatabindMapper();
         String serializedResponse = jsonMapper.writeValueAsString(response)
 
         then:
         serializedResponse == "{\"type\":\"connection_ack\",\"id\":\"test-id\",\"payload\":{\"test-key\":\"test-value\"}}"
+
+        where:
+        mapperType  | jsonMapper
+        'jackson'   | jacksonMapper
+        'micronaut' | micronautMapper
     }
 
-    void "test response deserialization using jackson object mapper"() {
+    void "test response deserialization using #mapperType object mapper"(String mapperType) {
         given:
         String response = "{\"type\":\"connection_ack\",\"id\":\"test-id\",\"payload\":{\"test-key\":\"test-value\"}}"
 
         when:
-        JsonMapper jsonMapper = new JacksonDatabindMapper();
         GraphQLApolloWsResponse deserializedResponse = jsonMapper.readValue(response, GraphQLApolloWsResponse)
 
         then:
@@ -70,5 +92,10 @@ class GraphQLApolloWsSerializationSpec extends Specification {
         deserializedResponse.getId() == "test-id"
         deserializedResponse.getPayload().getSpecification().size() == 1
         deserializedResponse.getPayload().getSpecification().get("test-key") == "test-value"
+
+        where:
+        mapperType  | jsonMapper
+        'jackson'   | jacksonMapper
+        'micronaut' | micronautMapper
     }
 }
