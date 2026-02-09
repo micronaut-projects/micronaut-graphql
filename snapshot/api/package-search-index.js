48e54540eff1d3235c5ea5dd8c356fb825f29efc
lissam1 <lissam1@users.noreply.github.com>
2026-02-09 15:52:05 +0000

    Deploying to gh-pages - 15:52:05

--- snapshot/api/package-search-index.js
+++ snapshot/api/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"io.micronaut.configuration.graphql"},{"l":"io.micronaut.configuration.graphql.ws"},{"l":"io.micronaut.graphql.info"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html","k":"18"},{"l":"io.micronaut.configuration.graphql"},{"l":"io.micronaut.configuration.graphql.ws"},{"l":"io.micronaut.graphql.info"}];updateSearchResults();
