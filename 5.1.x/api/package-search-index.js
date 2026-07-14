60559c6beb7138f0fb827e7ea0b19f3b8ce0a712
sdelamo <sdelamo@users.noreply.github.com>
2026-07-14 11:07:49 +0000

    Deploying to gh-pages - 11:07:49

---
+++ 5.1.x/api/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html","k":"18"},{"l":"io.micronaut.configuration.graphql"},{"l":"io.micronaut.configuration.graphql.ws"},{"l":"io.micronaut.graphql.info"}];updateSearchResults();
